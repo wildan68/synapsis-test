3f3a9f1044df590c104187b7767a71e50d1ea12a
Wildan Rizky <wildanrizkiyadi68@gmail.com>
2024-12-20 13:04:14 +0700

    add edge runtime

--- src/app/posts/detail/[id]/page.tsx
+++ src/app/posts/detail/[id]/page.tsx
@@ -2,6 +2,8 @@
 import dynamic from "next/dynamic"
 import React from "react"
 
+export const runtime = 'edge';
+
 const PostDetailContainer = dynamic(() => import("@/containers/post/root-container").then(mod => mod.PostDetailContainer), { ssr: false })
 
 const PostCreatePage = () => {
--- src/app/posts/edit/[id]/page.tsx
+++ src/app/posts/edit/[id]/page.tsx
@@ -2,6 +2,8 @@
 import dynamic from "next/dynamic"
 import React from "react"
 
+export const runtime = 'edge';
+
 const PostEditContainer = dynamic(() => import("@/containers/post/root-container").then(mod => mod.PostEditContainer), { ssr: false })
 
 const PostCreatePage = () => {
